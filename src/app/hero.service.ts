b96bbfc84be39bec4bfeaae3fc9b176effc9f1ca
Casey Heagerty <cheagerty@gmail.com>
2021-02-13 15:22:13 +0100

    lesson 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,8 +12,15 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
+    const heroes = of(HEROES)!;
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    const retour: Observable<Hero> = of(HEROES.find(hero => hero.id === id))!;
+    return retour;
   }
 }
